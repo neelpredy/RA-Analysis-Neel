d97ed7662af21badfe2fee9b25be61f23b55c736
Neel Pupreddiwar <neel_pupreddiwar@persistent.com>
2019-02-28 15:43:58 +0530

    hough

--- HoughCirles.py
+++ HoughCirles.py
@@ -1,21 +1,27 @@
 import cv2
 import numpy as np
 
-while True:
-    imageInput = cv2.imread("C:/Users/neel_pupreddiwar/Documents/GitHub/RA-detection/ra4.jpg",0)
-    
-    #convert to hsv
-    hsv_img = cv2.cvtColor(imageInput, cv2.COLOR_BGR2HSV)
 
-    #convert to threshhold
-    ret, thresh = cv2.threshold(hsv_img, 125, 255, 0)
+imageInput = cv2.imread("C:/Users/neel_pupreddiwar/Documents/GitHub/RA-detection/eye1.jpg",1)
+imgGray = cv2.cvtColor(imageInput,cv2.COLOR_BGR2GRAY)
+imgGray = cv2.medianBlur(imgGray, 5)
+cv2.imshow('bgr',imgGray)
 
-    #compute Hough circle
-    hough_image = cv2.HoughCircles(thresh, cv2.HOUGH_GRADIENT, 1, 20, param1=50, param2=30, minRadius=0, maxRadius=0)
-    
-    #print(hough_image)
-    cv2.imshow('output',hough_image)
+rows = imgGray.shape[0]
 
-    k = cv2.waitKey(1)
-    if k == 27:
-        break
+circles = cv2.HoughCircles(imgGray,cv2.HOUGH_GRADIENT,1,rows / 8,
+                        param1=50,param2=30,minRadius=0,maxRadius=0)
+
+circles = np.uint16(np.around(circles))
+for i in circles[0,:]:
+    center = (i[0], i[1])
+    # circle center
+    cv2.circle(imgGray, center, 1, (0, 100, 100), 3)
+    # circle outline
+    radius = i[2]
+    cv2.circle(imgGray, center, radius, (255, 0, 255), 3)
+
+cv2.imshow('detected circles',imgGray)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
